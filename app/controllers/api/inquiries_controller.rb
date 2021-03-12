75846fb08469402c9f34692afd3b6333020aee0e
Erin Cola <colaerin@yahoo.com>
2021-03-11 23:49:29 -0500

    created a notes file, built inquiry controller

--- app/controllers/api/inquiries_controller.rb
+++
@@ -1,2 +0,0 @@
-class Api::V1::InquiriesController < ApplicationController
-end
---
+++ app/controllers/api/v1/inquiries_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::InquiriesController < ApplicationController
+
+    def index
+        inquiries = Inquiry.all
+        render json: inquiries
+    end
+
+    def create
+        inquiry = Inquiry.new(inquiry_params)
+        if inquiry.save
+            render json: inquiry, status: :accepted
+        else 
+            render json: {errors: inquiry.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def inquiry_params
+        params.require(:inquiry).permit(:comment, :pet_id)
+    end
+
+end
