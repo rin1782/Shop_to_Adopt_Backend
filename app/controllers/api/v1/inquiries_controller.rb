5cb5d1778d37f94fe354ce00dddf2b0edf293832
Erin Cola <colaerin@yahoo.com>
2021-03-12 01:02:29 -0500

    created pets controller

--- app/controllers/api/v1/inquiries_controller.rb
+++ app/controllers/api/v1/inquiries_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::InquiriesController < ApplicationController
+    before_action :find_inquiry, only: [:update]
 
     def index
         inquiries = Inquiry.all
@@ -14,10 +15,23 @@ class Api::V1::InquiriesController < ApplicationController
         end
     end
 
+    def update
+        @inquiry.update(inquiry_params)
+        if @inquiry.save
+          render json: @inquiry, status: :accepted
+        else
+          render json: { errors: @inquiry.errors.full_messages }, status: :unprocessible_entity
+        end
+      end
+
     private
 
     def inquiry_params
         params.require(:inquiry).permit(:comment, :pet_id)
+    end
+
+    def find_inquiry
+        @inquiry = Inquiry.find(params[:id])
     end
 
 end
